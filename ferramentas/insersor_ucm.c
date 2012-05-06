152244141a6d8c6bfdac96f2b333a8d75ae5fff2
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2012-05-06 11:03:28 -0300

    Insersor de textos: fuga de memória
    Signed-off-by: Rodolfo Ribeiro Gomes <rodolforg@gmail.com>

--- ferramentas/insersor_ucm.c
+++ ferramentas/insersor_ucm.c
@@ -96,6 +96,7 @@ int main(int argc, char*argv[])
 	}
 	fecha_mensagens(mensagens, qtdMensagens);
 	salva_arquivo(nomeArquivoUCMNovo, bufferUCM, tamanhoUCM);
+	free(bufferUCM);
 	
 	exit(EXIT_SUCCESS);
 }
